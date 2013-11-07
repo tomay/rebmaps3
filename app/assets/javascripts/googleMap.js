8a1f75a11a5fbb422241b99c41ac34e94eb3046c
tomay <tom.allnutt@gmail.com>
2013-11-06 17:31:51 -0800

    Refactoring javascript to reflect views and controller * Adds new js file for cartoDB specific functions * adds new js file to hold contoller functions * renames views.js to view.js, holds view-related functions * additional modifications to existing js files and functions to organize and group like functions

--- app/assets/javascripts/googleMap.js
+++ app/assets/javascripts/googleMap.js
@@ -1,39 +1,55 @@
-var map;
+var googleMap = {
+  drawPolygon: function(poly) {
+    if (typeof polys === 'undefined') { polys = new Array() }
+    for (i in polys) { polys[i].setMap(null); }
+    poly = googleMap.makeGooglePolygon(poly)
+    poly.setMap(this.map);
+    polys.push(poly);
+  },
 
-function drawPolygon(poly) {
+  buildPolyFromCoords: function(coords) {
+    poly = new Array();
+    for (j in coords) {
+      poly.push(new google.maps.LatLng(coords[j][1], coords[j][0]))
+    }
+    poly.pop();
+    return poly
+  },
 
-  if (typeof polys === 'undefined') { polys = new Array() }
+  makeGooglePolygon: function(poly) {
+    return new google.maps.Polygon({
+      paths: poly, strokeColor: '#000000',
+      strokeOpacity: 0, strokeWeight: 2,
+      fillColor: "#FF6600",
+      fillOpacity: 0.9
+    });
+  },
 
-  for (i in polys) { polys[i].setMap(null); }
+  createNewMap: function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(-18.9,47.52),
+      zoom: 9,
+      disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.TOP_RIGHT
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false
+    });
 
-  poly = constructPolygon(poly)
-  poly.setMap(map);
-  polys.push(poly);
-}
+    this.map = map
+  },
 
-var constructPolygon = function(poly) {
-  return new google.maps.Polygon({
-  paths: poly, strokeColor: '#000000',
-  strokeOpacity: 0, strokeWeight: 2,
-  fillColor: "#FF6600",
-  fillOpacity: 0.9
-  });
-}
+  setCursorToPointer: function() {
+    this.map.setOptions({draggableCursor: 'pointer'});
+  },
 
-var createNewMap = function() {
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(-18.9,47.52),
-    zoom: 9,
-    disableDefaultUI: true,
-    zoomControl: true,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL,
-      position: google.maps.ControlPosition.TOP_RIGHT
-    },
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: false
-  });
-
-  return map 
+  setCenter: function(first_lat,last_lat,first_long,last_long) {
+    var lat_ave = ((parseFloat(first_lat.toFixed(4)) + parseFloat(last_lat.toFixed(4))) / 2);
+    var long_ave = ((parseFloat(first_long.toFixed(4)) + parseFloat(last_long.toFixed(4))) / 2);
+    var center = new google.maps.LatLng(lat_ave, long_ave)
+    googleMap.map.setCenter(center);
+  }
 }
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,10 @@
 $(document).ready(function() {
 
-  var map = createNewMap()
+  googleMap.createNewMap()
   View.createSelectorPane()
   View.createResultsPane()
 
-  $('.buttons button').click(switchEras)
+  $('.buttons button').click(Controller.switchEras)
 
 }) // doc ready
 
-var switchEras = function() {
-  var era = this.value
-  focusButton( this );
-  createMapLayer(era, map);
-  View.createMapKey(era);
-}
-
-var focusButton = function(button) {
-  $button = $( button )
-  $button.focus();
-  $button.closest('div').find('button.selected').removeClass('selected');
-  $button.addClass('selected');
-}
